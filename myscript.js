348746d8584aed96086fac3cffb40add245e38cc
Neeraj Singh <neerajdotname@gmail.com>
2010-02-17 10:44:47 -0500

    test page

--- myscript.js
+++ myscript.js
@@ -1,27 +1,32 @@
 //http://github.com/jquery/jquery/commit/da966573c3f3cb083c48cd3ead88e7542937d4df
-var linkify = function($) {
+$(function() {
 
-	var $elem = $('#commit .group .envelope .human .message pre');
-	var text = $elem.text();
+	var linkify = function($) {
 
-	if (!text) {
-		return;
-	}
+		var $elem = $('#commit .group .envelope .human .message pre');
+		var text = $elem.text();
 
-	var matches = text.match(/#\d{4}/g);
-	if (!matches) {
-		return;
-	}
+		if (!text) {
+			return;
+		}
 
-	$.map(matches, function(bugId) {
-		var url = 'http://dev.jquery.com/ticket/' + bugId;
-		$elem.append($('<a />', {
-			href: url,
-			text: '[#' + bugId + ']',
-      css: {color: 'green'}
-		}));
+		var matches = text.match(/#\d{4}/g);
+		if (!matches) {
+			return;
+		}
 
-	});
+		$.map(matches, function(bugId) {
+			var url = 'http://dev.jquery.com/ticket/' + bugId;
+			$elem.append($('<a />', {
+				href: url,
+				text: '[#' + bugId + ']',
+				css: {
+					color: 'green'
+				}
+			}));
 
-} (jQuery);
+		});
+
+	} (jQuery);
+});
 
--- test.js
+++
@@ -1,7 +0,0 @@
-var msg = "cleaned up. Fixes #6084.";
-
-msg = "A temporary fix for #5997, needs more investigation. ";
-
-msg = "Fixes #5856 and #5906. ";
-
-msg = "thanks to justin for this one. (Fixes ticket #5888) ";
