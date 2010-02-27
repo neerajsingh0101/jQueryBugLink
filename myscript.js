f4cccf920098e0b75da8a64b0221483f5adadc25
mofle <mofle89@gmail.com>
2010-02-27 18:18:39 +0100

    Instead of appending the link, I've changed it so that it replaces the ticket id with a link.

--- myscript.js
+++ myscript.js
@@ -1,30 +1,31 @@
 $(function() {
-
+ 
 	var $elem = $('#commit .group .envelope .human .message pre'),
 	text = $elem.text(),
 	matches = [],
 	regex = /#(\d+)/g;
-
+ 
 	if (!text) {
 		return;
 	}
-
+ 
 	while ((match = regex.exec(text)) !== null) {
 		matches.push(match[1]);
 	}
-
+ 
 	if (!matches.length) {
 		return;
 	}
-
+ 
 	$.map(matches, function(bugId) {
-		$elem.append($('<a />', {
+		var $bugLink = $('<a />', {
 			href: 'http://dev.jquery.com/ticket/' + bugId,
-			text: '[#' + bugId + ']',
+			text: '#' + bugId,
 			css: {
-				color: 'green'
+				color: '#4183C4'
 			}
-		}));
+		});	
+		$elem.html($elem.html().replace('#'+bugId, $bugLink[0].outerHTML));
 	});
+	
 });
-
