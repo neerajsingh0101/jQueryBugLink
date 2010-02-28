e0b58f3501c6f928b159ff27b605d6a10764d6e8
Neeraj Singh <neerajdotname@gmail.com>
2010-02-27 23:05:46 -0500

    outerHTML did not work in firefox. fixing it.

--- myscript.js
+++ myscript.js
@@ -1,22 +1,23 @@
 $(function() {
- 
+
 	var $elem = $('#commit .group .envelope .human .message pre'),
 	text = $elem.text(),
 	matches = [],
+	outerHtml,
 	regex = /#(\d+)/g;
- 
+
 	if (!text) {
 		return;
 	}
- 
+
 	while ((match = regex.exec(text)) !== null) {
 		matches.push(match[1]);
 	}
- 
+
 	if (!matches.length) {
 		return;
 	}
- 
+
 	$.map(matches, function(bugId) {
 		var $bugLink = $('<a />', {
 			href: 'http://dev.jquery.com/ticket/' + bugId,
@@ -24,8 +25,10 @@ $(function() {
 			css: {
 				color: '#4183C4'
 			}
-		});	
-		$elem.html($elem.html().replace('#'+bugId, $bugLink[0].outerHTML));
+		});
+		outerHtml = $('<div>').append($bugLink.clone()).html();
+		$elem.html($elem.html().replace('#' + bugId, outerHtml));
 	});
-	
+
 });
+
